6e0a682182af54a40f3fb05f612da6df25c1e6d8
george <971462342@qq.com>
2023-02-27 01:09:52 +0800

    test

---
+++ src/OnJava8/ch11/innerclasses/LocalInnerClass.java
@@ -0,0 +1,59 @@
+package OnJava8.ch11.innerclasses;
+
+interface Counter {
+    int next();
+}
+
+// 唯一的理由是，我们需要一个已命名的构造器，或者需要重载构造器，而匿名内部类只能用于实例初始化。
+public class LocalInnerClass {
+    private int count = 0;
+
+    // A local inner class:
+    Counter getCounter(final String name) {
+        // A local inner class
+        class LocalCounter implements Counter {
+
+            LocalCounter() {
+                // Local inner class can have a constructor
+                System.out.println("LocalCounter()");
+            }
+
+            @Override
+            public int next() {
+                System.out.println(name);
+                return count++;
+            }
+        }
+        return new LocalCounter();
+    }
+
+    // Repeat, but with an anonymous inner class:
+    Counter getCounter2(final String name) {
+        return new Counter() {
+            // Anonymous inner class cannot have a named
+            // constructor, only an instance initializer:
+            {
+                System.out.println("Counter()");
+            }
+
+            @Override
+            public int next() {
+                System.out.println(name);
+                return count++;
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        LocalInnerClass lic = new LocalInnerClass();
+        Counter
+                c1 = lic.getCounter("Local inner"),
+                c2 = lic.getCounter2("Anonymous inner");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(c1.next());
+        }
+        for (int i = 0; i < 5; i++) {
+            System.out.println(c2.next());
+        }
+    }
+}
