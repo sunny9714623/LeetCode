03dd407f551320a676215bc11d48b1b22685b99e
george <971462342@qq.com>
2023-03-22 01:36:56 +0800

    Exception 操作

---
+++ src/OnJava8/ch15/exceptions/WhoCalled.java
@@ -0,0 +1,29 @@
+package OnJava8.ch15.exceptions;
+
+public class WhoCalled {
+    static void f() {
+        try {
+            throw new Exception();
+        } catch (Exception e) {
+            for (StackTraceElement ste : e.getStackTrace()) {
+                System.out.println(ste.getMethodName());
+            }
+        }
+    }
+
+    static void g() {
+        f();
+    }
+
+    static void h() {
+        g();
+    }
+
+    public static void main(String[] args) {
+        f();
+        System.out.println("***********");
+        g();
+        System.out.println("***********");
+        h();
+    }
+}
